715fb6d42c4ce5c25b432dd791d4ead8423fe816
JeongHyeon Kim <jeonghyeon.kim@creatrip.com>
2022-05-06 19:32:42 +0900

    fix: lint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,4 +44,5 @@ module.exports = {
       },
     },
   ],
+  ignorePatterns: ['.eslintrc.js'],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import 'reflect-metadata';
 import * as fs from 'fs';
 import path from 'path';
 import ini from 'ini';
 
-export const INIStorage = new Map<string, { name: string; type: Function }[]>();
+export const INIStorage: Map<
+  string,
+  {
+    name: string;
+    type: Function;
+  }[]
+> = new Map<string, { name: string; type: Function }[]>();
 
 /**
  * 값을 들고 오겠다고 정의하는 데코레이터입니다.
@@ -17,12 +24,17 @@ export const INIStorage = new Map<string, { name: string; type: Function }[]>();
  * ```
  */
 export function Column(): PropertyDecorator {
-  return (object: any, propertyName: string) => {
-    const reflectMetadataType =
+  return (object: any, propertyName: string): void => {
+    const reflectMetadataType: any =
       Reflect && Reflect.getMetadata
         ? Reflect.getMetadata('design:type', object, propertyName)
         : undefined;
-    let storage = INIStorage.get(object.name);
+    let storage:
+      | {
+          name: string;
+          type: Function;
+        }[]
+      | undefined = INIStorage.get(object.name);
     if (storage === undefined) {
       storage = [];
       INIStorage.set(object.name, storage);
@@ -52,16 +64,24 @@ export function LoadFile(filename?: string, options?: ILoadFileOptions): ClassDe
     filename += '.ini';
   }
 
-  return (object: any) => {
-    const parsedData = ini.parse(
+  return (object: any): void => {
+    const parsedData: {
+      [key: string]: any;
+    } = ini.parse(
       fs.readFileSync(path.join(process.cwd(), `${filename}`), {
         encoding: 'utf-8',
       }),
     );
-    const columns = INIStorage.get(object.name);
+    const columns: {
+      name: string;
+      type: Function;
+    }[] = INIStorage.get(object.name);
     if (columns) {
       for (const index in parsedData) {
-        const column = columns.find((column) => column.name === index);
+        const column: {
+          name: string;
+          type: Function;
+        } = columns.find((column: { name: string; type: Function }) => column.name === index);
 
         // Validation - INI에는 정의되어있는데, Class에는 정의되어있지 않으면 에러 발생
         if (column === undefined) {
